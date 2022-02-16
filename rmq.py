af7ffe54911bbd31a07531081ad45c0dc1356f47
kevinthedang <kevinthedang_1@outlook.com>
2022-02-15 16:46:47 -0800

    established connection

--- rmq.py
+++ rmq.py
@@ -1,44 +1,31 @@
 import pika
 
 GET_ALL_MESSAGES = -1
-RMQ_HOST = 'cps-devops.gonzaga'
+RMQ_HOST = 'cps-devops.gonzaga.edu'
 RMQ_PORT = 5672
 RMQ_USER = 'class'
 RMQ_PASS = 'CPSC313'
 RMQ_DEFAULT_PUBLIC_QUEUE = 'general'
 RMQ_DEFAULT_PRIVATE_QUEUE = ''
+RMQ_TEST_HOST = 'localhost'
+RMQ_DEFAULT_VH = '/'
 
-connection =  pika.BlockingConnection(pika.ConnectionParameters('http://cps-devops.gonzaga.edu:5672'))
+credentials = pika.PlainCredentials(RMQ_USER, RMQ_PASS)
+parameters = pika.ConnectionParameters(host=RMQ_HOST, port=RMQ_PORT, virtual_host=RMQ_DEFAULT_VH, credentials=credentials, heartbeat=6000)
+connection =  pika.BlockingConnection(parameters=parameters)
 channel = connection.channel()
 
-def on_connected(connection):
-    connection.channel(on_open_callback=on_channel_open)
+channel.queue_declare(queue=RMQ_DEFAULT_PUBLIC_QUEUE)
+channel.basic_publish(exchange='', routing_key='general', body='hello kevin')
+print(" [x] Sent 'hello kevin'")
 
-# when the channel is open, this is called
-def on_channel_open(new_channel):
-    global channel
-    channel = new_channel
-    channel.queue_declare(queue="test", durable=True, exclusive=False, auto_delete=False, callback=on_queue_declared)
+connection.close()
 
-def on_queue_declared(frame):
-    channel.basic_consume('test', handle_delivery)
-
-def handle_delivery(channel, method, header, body):
-    """Called when we receive a message from RabbitMQ"""
-    print(body)
-
-# Step #1: Connect to RabbitMQ using the default parameters
-parameters = pika.ConnectionParameters()
-connection = pika.SelectConnection(parameters, on_open_callback=on_connected)
-
-try:
-    # Loop so we can communicate with RabbitMQ
-    connection.ioloop.start()
-except KeyboardInterrupt:
-    # Gracefully close the connection
-    connection.close()
-    # Loop until we're fully closed, will stop on its own
-    connection.ioloop.start()
+class RMQConnection():
+    '''
+    '''
+    def __init__(self) -> None:
+        pass
 
 class MessageServer():
     '''
@@ -47,8 +34,9 @@ class MessageServer():
         ''' Set up all the RMQ settings
             Create and set up connection, channels, queues, exchanges, etc.
         '''
-        self.__connection = pika.create_connection()
+        # self.__connection = pika.create_connection()
         # self.__public_exchange = pika.create_exchange()
+        pass
 
     def send_message(self, message: str, target_queue: str) -> bool:
         ''' 
