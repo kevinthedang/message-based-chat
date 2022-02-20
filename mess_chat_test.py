d3af34db41df8f2d85c462262987bafd8da2c616
kevinthedang <kevinthedang_1@outlook.com>
2022-02-19 19:23:15 -0800

    api test

--- mess_chat_test.py
+++ mess_chat_test.py
@@ -1,11 +1,10 @@
 import unittest
-import mess_chat
 import requests
-import pytest
  
-SERVER_URL = 'http://cps-devops.gonzaga.edu:5672/'
 STARTUP_URL = 'http://localhost:8000/'
-RECEIVE_URL = 'http://127.0.0.1:8000/messages/?=message_count=1&queue_destination=general'
+SEND_URL = 'http://localhost:8000/send/'
+RECEIVE_URL = 'http://127.0.0.1:8000/messages/'
+PUBLIC_QUEUE = 'general'
 
 class APITest(unittest.TestCase):
     """
@@ -17,16 +16,18 @@ class APITest(unittest.TestCase):
         ''' This test will test the startup responce when running uvicorn.
         '''
         startup = requests.get(STARTUP_URL)
-        print(APITest.book)
         self.assertEqual(startup.status_code, 200)
 
 
     def test_send(self):
         ''' This test will test the sending of a message using query parameters.
-            Cases:
         '''
+        send_message = requests.post(SEND_URL, data = {'message' : 'Send API Test!', 'target_queue' : PUBLIC_QUEUE})
+        self.assertEqual(send_message.status_code, 200)
 
     def test_messages(self):
         ''' This test will test the receiving of messages from the server using query parameters.
-            Cases:
+            Cases: 1 Message
         '''
+        receive_message = requests.get(RECEIVE_URL, data = {'message_cout' : 1, 'queue_destination' : PUBLIC_QUEUE})
+        self.assertEqual(receive_message.status_code, 200)
